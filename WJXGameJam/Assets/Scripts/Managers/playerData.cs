bf1e26d53788f1b668882cae5e1dc49adae053b7
AlwinMoo <geraldcheu@gmail.com>
2020-08-02 22:33:43 +0800

    Add simple player data script

---
+++ WJXGameJam/Assets/Scripts/Managers/playerData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class playerData
+{
+    public static List<int> moneyPerDay = new List<int>();
+    public static List<int> dishesPerDay = new List<int>();
+
+    public static int GetTotalMoney()
+    {
+        int temptotal = 0;
+        foreach (var money in moneyPerDay)
+        {
+            temptotal += money;
+        }
+        return temptotal;
+    }
+
+    public static int GetTotalDishes()
+    {
+        int temptotal = 0;
+        foreach (var dishes in moneyPerDay)
+        {
+            temptotal += dishes;
+        }
+        return temptotal;
+    }
+}
