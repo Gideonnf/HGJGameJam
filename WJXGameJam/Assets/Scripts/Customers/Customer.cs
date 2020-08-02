7ad377cca8a71ee8020fcfd0a4d77e22504a0524
Angie <173123N@mymail.nyp.edu.sg>
2020-08-02 23:14:49 +0800

    - customers can go in and out and select a queue position

--- WJXGameJam/Assets/Scripts/Customers/Customer.cs
+++ WJXGameJam/Assets/Scripts/Customers/Customer.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class Customer : MonoBehaviour
 {
+    float m_WalkSpeed = 5.0f;
+
     float m_PatienceTime = 0.0f;
     float m_PatienceTimeTracker = 0.0f;
 
-    public void Init(float patienceTime, Vector2 spawnPos)
+    Vector2 m_QueuePos = Vector2.zero;
+    bool m_AtQueuePos = false;
+
+    Vector2 m_ExitPos = Vector2.zero;
+
+    Vector2 m_WalkDir = Vector2.zero;
+    bool m_LeavingStall = false;
+
+    public delegate void OnLeftStall();
+    public OnLeftStall OnLeftStallCallback;
+
+    public void Init(float patienceTime, Vector2 spawnPos, Vector2 queuePos, Vector2 exitPos)
     {
         m_PatienceTime = patienceTime;
         transform.position = spawnPos;
+        m_QueuePos = queuePos;
+        m_ExitPos = exitPos;
 
         m_PatienceTimeTracker = 0.0f;
-
-        //TODO:: also init a walk position to go too
+        m_AtQueuePos = false;
+        m_WalkDir = (queuePos - spawnPos).normalized;
+        m_LeavingStall = false;
 
         CreateFoodOrder();
     }
@@ -26,26 +42,70 @@ public class Customer : MonoBehaviour
 
     public void Update()
     {
-        m_PatienceTimeTracker += Time.fixedDeltaTime;
-
-        //TODO:: have own sprite states for impatient, unhappy, about to walk away, and walking away
-
-
-        if (m_PatienceTimeTracker > m_PatienceTime)
+        //for when leaving the stall
+        if (m_LeavingStall)
         {
             WalkAway();
+            return;
         }
+
+        //when customer walking to the queue slot
+        if (!m_AtQueuePos)
+        {
+            WalkToQueue(); //walk to queue
+            return;
+        }
+
+        //TODO:: have own sprite states for impatient, unhappy, about to walk away, and walking away
+        m_PatienceTimeTracker += Time.fixedDeltaTime;
+        if (m_PatienceTimeTracker > m_PatienceTime)
+        {
+            LeavingStall();
+        }
+    }
+
+    public void WalkToQueue()
+    {
+        transform.position += (Vector3)(m_WalkDir * Time.fixedDeltaTime * m_WalkSpeed);
+
+        //check if it overshot the position
+        Vector2 nextDir = m_QueuePos - (Vector2)transform.position;
+        nextDir.Normalize();
+        if (Vector2.Dot(nextDir, m_WalkDir) < 0)
+        {
+            m_AtQueuePos = true;
+            transform.position = m_QueuePos;
+        }
+    }
+
+    public void LeavingStall()
+    {
+        m_LeavingStall = true;
+        m_WalkDir = (m_ExitPos - (Vector2)transform.position).normalized;
     }
 
     public void WalkAway()
     {
-        //TODO:: walk away to one side of the screen
+        //walk away to one side of the screen
+        transform.position += (Vector3)(m_WalkDir * Time.fixedDeltaTime * m_WalkSpeed);
+
+        //check if it overshot the position
+        Vector2 nextDir = m_ExitPos - (Vector2)transform.position;
+        nextDir.Normalize();
+        if (Vector2.Dot(nextDir, m_WalkDir) < 0)
+        {
+            gameObject.SetActive(false);
+            OnLeftStallCallback.Invoke();
+        }
     }
 
     public void CheckFood()
     {
         //TODO:: check if food is correct, walk away accordingly
         //if food correct pay, if not correct, pay only like a certain percentage of it
+        //add to the customer serve
+
+        LeavingStall();
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
--- WJXGameJam/Assets/Scripts/Customers/CustomerManager.cs
+++ WJXGameJam/Assets/Scripts/Customers/CustomerManager.cs
@@ -5,19 +5,116 @@ using UnityEngine;
 public class CustomerManager : MonoBehaviour
 {
     //when customer spawn, make sure spawn them at the correct positions
-    [Header("Customers")]
+    [Header("Customers Details")]
     public CustomerObjectPooler m_CustomerObjPooler = new CustomerObjectPooler();
+    public Transform m_CustomerQueuePosParent = null;
+    public Transform m_CustomerEnterExitPosParent = null;
 
+    List<Transform> m_CustomerQueuePosList = new List<Transform>();
+    List<Transform> m_CustomerEnterExitPosList = new List<Transform>();
 
+    GameObject[] m_CustomerQueuing;
+    int m_CurrentCustomersInQueue = 0;
+    int m_MaxCustomerInQueue = 3;
 
     void Awake()
     {
         m_CustomerObjPooler.Init();
+
+        if (m_CustomerQueuePosParent != null)
+        {
+            foreach(Transform queuePos in m_CustomerQueuePosParent)
+            {
+                m_CustomerQueuePosList.Add(queuePos);
+            }
+        }
+
+        if (m_CustomerEnterExitPosParent != null)
+        {
+            foreach (Transform enterExitPos in m_CustomerEnterExitPosParent)
+            {
+                m_CustomerEnterExitPosList.Add(enterExitPos);
+            }
+        }
+
+        //dealing with customer in queue
+        m_MaxCustomerInQueue = m_CustomerQueuePosList.Count;
+        m_CurrentCustomersInQueue = 0;
+
+        m_CustomerQueuing = new GameObject[m_MaxCustomerInQueue];
+        for (int i = 0; i < m_MaxCustomerInQueue; ++i) //make sure its fully null
+        {
+            m_CustomerQueuing[i] = null;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (m_CurrentCustomersInQueue < m_MaxCustomerInQueue)
+        {
+            GetNewCustomerToQueue();
+        }
+    }
+
+    public void GetNewCustomerToQueue()
+    {
+        GameObject customerObj = m_CustomerObjPooler.GetCustomerFromPooler();
+        if (customerObj == null)
+            return;
+
+        Customer customer = customerObj.GetComponent<Customer>();
+        if (customer == null)
+            return;
+
+        //init customer positions
+        if (m_CustomerEnterExitPosList.Count <= 0)
+            return;
+
+        Vector2 enterPos = m_CustomerEnterExitPosList[Random.Range(0, m_CustomerEnterExitPosList.Count)].position;
+        Vector2 exitPos = m_CustomerEnterExitPosList[Random.Range(0, m_CustomerEnterExitPosList.Count)].position;
+
+        //find place for the new customer
+        for (int i = 0; i < m_MaxCustomerInQueue; ++i)
+        {
+            if (m_CustomerQueuing[i] != null)
+                continue;
+
+            //initialise queuePos
+            Vector2 queuePos = Vector2.zero;
+            if (i < m_CustomerQueuePosList.Count)
+                queuePos = m_CustomerQueuePosList[i].position;
+
+            customerObj.SetActive(true);
+            customer.Init(5.0f, enterPos, queuePos, exitPos); //TODO:: change speed based on progress
+            customer.OnLeftStallCallback += CustomerLeave;
+
+            m_CustomerQueuing[i] = customerObj;
+            ++m_CurrentCustomersInQueue;
+
+            break;
+        }
+    }
+
+    public void CustomerLeave()
+    {
+        //check which customer is inactive
+        for (int i =0; i < m_MaxCustomerInQueue; ++i)
+        {
+            if (m_CustomerQueuing[i] == null)
+                continue;
+
+            if (m_CustomerQueuing[i].activeSelf)
+                continue;
+
+            Customer customer = m_CustomerQueuing[i].GetComponent<Customer>();
+            if (customer != null)
+                customer.OnLeftStallCallback -= CustomerLeave; //unsubscribe the listener
+
+            m_CustomerQueuing[i] = null;
+            --m_CurrentCustomersInQueue;
+
+            break;
+        }
     }
 }
