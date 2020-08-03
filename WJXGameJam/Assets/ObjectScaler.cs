057ac3a9054b5df51955ed99be6f6903ab7727f9
Gideon Francis <gideonnfrancis999@gmail.com>
2020-08-04 02:19:42 +0800

    Fixed resolution stuff with angie

--- WJXGameJam/Assets/ObjectScaler.cs
+++ WJXGameJam/Assets/ObjectScaler.cs
@@ -6,6 +6,8 @@ public class ObjectScaler : MonoBehaviour
 {
     public GameObject BackgroundObject;
 
+    private Vector2 resolution;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +19,28 @@ public class ObjectScaler : MonoBehaviour
         float unitHeight = s.textureRect.height / s.pixelsPerUnit;
 
         transform.localScale = new Vector3(width / unitWidth, height / unitHeight);
+
+        resolution = new Vector2(Screen.width, Screen.height);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (resolution.x != Screen.width || resolution.y != Screen.height)
+        {
+            // do your stuff
+            float height = Camera.main.orthographicSize * 2;
+            float width = height * Screen.width / Screen.height; // basically height * screen aspect ratio
+
+            Sprite s = BackgroundObject.GetComponent<SpriteRenderer>().sprite;
+            float unitWidth = s.textureRect.width / s.pixelsPerUnit;
+            float unitHeight = s.textureRect.height / s.pixelsPerUnit;
+
+            transform.localScale = new Vector3(width / unitWidth, height / unitHeight);
+
+
+            resolution.x = Screen.width;
+            resolution.y = Screen.height;
+        }
     }
 }
