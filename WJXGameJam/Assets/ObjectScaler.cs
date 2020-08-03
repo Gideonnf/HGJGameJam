eecef47a61d4e4b7fa21fe9f445457f065cb7bb5
Gideon Francis <gideonnfrancis999@gmail.com>
2020-08-04 01:45:12 +0800

    Added object scaler script

---
+++ WJXGameJam/Assets/ObjectScaler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScaler : MonoBehaviour
+{
+    public GameObject BackgroundObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float height = Camera.main.orthographicSize * 2;
+        float width = height * Screen.width / Screen.height; // basically height * screen aspect ratio
+
+        Sprite s = BackgroundObject.GetComponent<SpriteRenderer>().sprite;
+        float unitWidth = s.textureRect.width / s.pixelsPerUnit;
+        float unitHeight = s.textureRect.height / s.pixelsPerUnit;
+
+        transform.localScale = new Vector3(width / unitWidth, height / unitHeight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
