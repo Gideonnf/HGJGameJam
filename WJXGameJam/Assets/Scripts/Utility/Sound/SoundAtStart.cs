9d1449dd770b5df20ef15130bb5efacdcb03e3cf
Angie <173123N@mymail.nyp.edu.sg>
2020-08-07 01:38:00 +0800

    - ambient noise added

---
+++ WJXGameJam/Assets/Scripts/Utility/Sound/SoundAtStart.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SoundAtStart : MonoBehaviour
+{
+    public string m_BackgroundSound = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SoundManager.Instance != null)
+            SoundManager.Instance.Play(m_BackgroundSound);
+    }
+}
